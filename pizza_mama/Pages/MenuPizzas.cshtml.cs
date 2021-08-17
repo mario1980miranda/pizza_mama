bf61bca6f38f652192bd58857ea526e326d20d3a
Mario Luiz Miranda <mario_chan15@hotmail.com>
2021-08-17 19:46:20 -0400

    Rajouter une feuille de style (CSS)

--- pizza_mama/Pages/MenuPizzas.cshtml.cs
+++ pizza_mama/Pages/MenuPizzas.cshtml.cs
@@ -4,13 +4,26 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using pizza_mama.Data;
+using pizza_mama.Models;
 
 namespace pizza_mama.Pages.Admin
 {
     public class MenuPizzasModel : PageModel
     {
-        public void OnGet()
+        private readonly pizza_mama.Data.DataContext _context;
+
+        public IList<Pizza> Pizza { get; set; }
+
+        public MenuPizzasModel(DataContext context)
         {
+            _context = context;
+        }
+
+        public async Task OnGetAsync()
+        {
+            Pizza = await _context.Pizzas.ToListAsync();
         }
     }
 }
