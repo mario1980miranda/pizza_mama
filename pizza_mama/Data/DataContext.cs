71d58725e504cd5936b9cdb509b00e50834bf629
Mario Luiz Miranda <mario_chan15@hotmail.com>
2021-08-17 10:58:02 -0400

    Adicionar arquivos de projeto.

---
+++ pizza_mama/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using pizza_mama.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pizza_mama.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Pizza> Pizzas { get; set; }
+    }
+}
